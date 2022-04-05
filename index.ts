4db7e4850bad53f606d1becec090f0e1442f82a5
mafealno <mafealno@gmail.com>
2022-04-05 11:08:02 -0300

    feat: adicionado para validar conexao com telefone

--- index.ts
+++ index.ts
@@ -5,10 +5,13 @@ const start = async () => {
     const client = await create({
         session: 'wpp-bot',
         autoClose: 0,
+        disableWelcome: true,
         puppeteerOptions: { args: ['--no-sandbox'] },
     });
 
     client.onMessage(message => resolveMessage(message, client));
+
+    client.startPhoneWatchdog(10000);
 }
 
 start();
