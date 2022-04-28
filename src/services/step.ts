2bf8b7a0de6a8182bfe34b6e25cc847197a360ce
mafealno <mafealno@gmail.com>
2022-04-28 19:50:16 -0300

    remove: Retirado salvar steps

--- src/services/step.ts
+++ src/services/step.ts
@@ -1,15 +1,13 @@
 import LastInteraction from "../types/lastInteraction";
 import * as stepsRespository from "../repositories/step";
-import { Step } from "../types/step";
-const stepsSave = require("../../passos.json"); 
+import { Step } from "../types/step"; 
 
 export const getStep = async (stepId: string, lastInteraction? : LastInteraction) : Promise<Step> => {
-    //saveSteps(stepsSave)
     const steps = await getSteps()
     return steps.find(step => step.id == (lastInteraction ? lastInteraction.nextStep : stepId));
 }
 
-const saveSteps = async (steps: Array<Step>) => {
+export const saveSteps = async (steps: Array<Step>) => {
     return await stepsRespository.saveSteps(steps);
 }
 
