d09bdf77e29e847b4238edd1531ec92a52e2a259
mafealno <mafealno@gmail.com>
2022-04-28 19:51:23 -0300

    alter: alterações para envio de email e salvar token

--- index.ts
+++ index.ts
@@ -1,17 +1,36 @@
 import { create } from "@wppconnect-team/wppconnect";
+import { sendMailConnect, sendMailDisconnect } from "./src/services/mail";
 import { resolveMessage } from "./src/services/message";
+import { saveSteps } from "./src/services/step";
+const stepsJson = require("./passos.json"); 
+
 
 const start = async () => {
     const client = await create({
         session: 'wpp-bot',
         autoClose: 0,
         disableWelcome: true,
-        puppeteerOptions: { args: ['--no-sandbox'] },
+        statusFind : async (statusSession, session) => {
+            switch(statusSession){
+                case "isLogged":
+                    await sendMailConnect();
+                    break;
+                case "notLogged":
+                    await sendMailDisconnect();
+                    break;
+                default:
+                    console.log(statusSession);
+            }
+        },
+        puppeteerOptions: { 
+            userDataDir: './tokens/wpp-bot',
+        }
     });
-
+    
     client.onMessage(message => resolveMessage(message, client));
-
-    client.startPhoneWatchdog(10000);
+    
+    client.startPhoneWatchdog();
 }
 
 start();
+//saveSteps(stepsJson);
