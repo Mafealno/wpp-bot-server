8d75cafa9999313c845a2af174bcd368c8adc402
mafealno <mafealno@gmail.com>
2022-09-16 21:04:37 -0300

    fix: retirada a funcção de fazer o qrCode aparecer no console

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
+require('dotenv').config();
 import { create } from "@wppconnect-team/wppconnect";
+import { saveSteps } from "./src/repositories/step";
 import { sendMailConnect, sendMailDisconnect } from "./src/services/mail";
 import { resolveMessage } from "./src/services/message";
 import { resolveQrCode } from "./src/services/qrCode";
-import { saveSteps } from "./src/services/step";
 const stepsJson = require("./passos.json"); 
-
 
 const start = async () => {
     const client = await create({
@@ -12,6 +12,7 @@ const start = async () => {
         autoClose: 0,
         disableWelcome: true,
         catchQR: (base64Qr) => resolveQrCode(base64Qr),
+        logQR: false, //faz o qrCode não ser exibido no console
         statusFind : async (statusSession, session) => {
             if(!process.env.DEVELOPMENT_ENVIRONMENT){
                 switch(statusSession){
@@ -37,11 +38,5 @@ const start = async () => {
     client.onMessage(message => resolveMessage(message, client));
     return client;
 }
-
-start().then((data) => {
-    setInterval(() => {
-        data.startPhoneWatchdog()
-        console.log("CONEXÃO VERIFICADA")
-    }, 10000)
-});
-//saveSteps(stepsJson);
+saveSteps(stepsJson);
+start()
