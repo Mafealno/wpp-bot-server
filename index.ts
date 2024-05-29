30217bb0973aafca6c3ec4d845f060cbb3a360b5
Marcelo Fernando Alves Nogueira <marcelo.nogueira@epharma.com.br>
2024-05-29 12:19:40 -0300

    fix

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ const start = async () => {
         autoClose: 0,
         disableWelcome: true,
         catchQR: (base64Qr) => resolveQrCode(base64Qr),
-        logQR: false, //faz o qrCode não ser exibido no console
+        logQR: true, //faz o qrCode não ser exibido no console
         statusFind : async (statusSession, session) => {
             if(!process.env.DEVELOPMENT_ENVIRONMENT){
                 switch(statusSession){
